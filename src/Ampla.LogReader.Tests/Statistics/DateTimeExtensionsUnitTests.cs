31137e364e67cd9f92e4ed5d932591892a481f0d
Joseph Plant <git@JosephPlant.com>
2014-08-20 18:43:04 +1000

    DateTimeExtensions for truncating local times

---
+++ src/Ampla.LogReader.Tests/Statistics/DateTimeExtensionsUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Ampla.LogReader.Statistics
+{
+    [TestFixture]
+    public class DateTimeExtensionsUnitTests : TestFixture
+    {
+        [Test]
+        public void LocalTime()
+        {
+            DateTime now = DateTime.Now;
+            Assert.That(now.Kind, Is.EqualTo(DateTimeKind.Local));
+            Assert.That(now.TimeOfDay, Is.GreaterThan(TimeSpan.Zero));
+
+            DateTime result = now.TruncateToLocalDay();
+            Assert.That(result, Is.EqualTo(now.Subtract(now.TimeOfDay)));
+            Assert.That(result.Kind, Is.EqualTo(DateTimeKind.Local));
+        }
+
+        [Test]
+        public void UtcTime()
+        {
+            DateTime utcNow = DateTime.UtcNow;
+            DateTime local = utcNow.ToLocalTime();
+
+            Assert.That(utcNow.Kind, Is.EqualTo(DateTimeKind.Utc));
+            Assert.That(local.TimeOfDay, Is.GreaterThan(TimeSpan.Zero));
+
+            DateTime result = utcNow.TruncateToLocalDay();
+            Assert.That(result, Is.EqualTo(utcNow.Subtract(local.TimeOfDay)));
+            Assert.That(result.Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+    }
+}
---
+++ src/Ampla.LogReader/Statistics/DateTimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ampla.LogReader.Statistics
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Truncates the DateTime to the day it occurs on.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns></returns>
+        public static DateTime TruncateToLocalDay(this DateTime dateTime)
+        {
+            DateTime local = dateTime.ToLocalTime();
+            TimeSpan timeOffset = local.TimeOfDay;
+            return dateTime.Subtract(timeOffset);
+        }
+    }
+}
