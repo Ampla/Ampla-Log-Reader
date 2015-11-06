7ae3dca320d813c55c6002fecfd92dfef900d4d2
Joseph Plant <git@JosephPlant.com>
2015-11-06 13:03:18 +1000

    Updated to include a directory of EventLog.evtx files

--- src/Ampla.LogReader.EventLog.Console/EventLogReaderOptions.cs
+++ src/Ampla.LogReader.EventLog.Console/EventLogReaderOptions.cs
@@ -4,13 +4,16 @@ namespace Ampla.LogReader.EventLog.Console
 {
     public class EventLogReaderOptions
     {
-        [Option('o', "output", Required = false, HelpText = "Select the Output file", DefaultValue = "EventLog.Details.xslx")]
+        [Option('o', "output", Required = false, HelpText = "Select the Output file", DefaultValue = "EventLog.Report.xslx")]
         public string OutputFile { get; set; }
 
-        [Option('f', "file", Required = true, HelpText = "Event Log file to parse", DefaultValue = "EventLog.evtx")]
+        [Option('f', "file", Required = false, HelpText = "Event Log file to parse", DefaultValue = "EventLog.evtx")]
         public string EventLogFile { get; set; }
 
-        [Option('z', "timezone", HelpText = "TimeZone for local date times", DefaultValue = null)]
-        public string TimeZone { get; set; }
+        [Option('d', "dir", Required = false, HelpText = "Event Log directory to parse for Event Log files (*.evtx)", DefaultValue = null)]
+        public string EventLogDirectory { get; set; }
+
+//        [Option('z', "timezone", HelpText = "TimeZone for local date times", DefaultValue = null)]
+//        public string TimeZone { get; set; }
     }
 }
--- src/Ampla.LogReader.EventLog.Console/Program.cs
+++ src/Ampla.LogReader.EventLog.Console/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Xml;
 using Ampla.LogReader.EventLogs;
-using Ampla.LogReader.FileSystem;
-using Ampla.LogReader.ReportWriters;
 using Ampla.LogReader.Reports.Packs;
-using Ampla.LogReader.Statistics;
+using CommandLine.Text;
 
 namespace Ampla.LogReader.EventLog.Console
 {
@@ -19,36 +13,45 @@ namespace Ampla.LogReader.EventLog.Console
             EventLogReaderOptions options = new EventLogReaderOptions();
             if (CommandLine.Parser.Default.ParseArgumentsStrict(args, options))
             {
-                using (IReportWriter reportWriter = GetReportWriter(options))
+                TextWriter writer = System.Console.Out;
+
+                writer.WriteLine("EventLogFile: {0}", options.EventLogFile);
+                writer.WriteLine("EventLogDirectory: {0}", options.EventLogDirectory);
+                writer.WriteLine("OutputFile: {0}", options.OutputFile);
+
+                bool fileExists = File.Exists(options.EventLogFile);
+                bool dirExists = !string.IsNullOrEmpty(options.EventLogDirectory) &&
+                                 Directory.Exists(options.EventLogDirectory);
+                string[] files = new string[0];
+                
+                if (dirExists)
                 {
-                    System.Console.WriteLine("EventLogFile: {0}", options.EventLogFile);
-                    System.Console.WriteLine("OutputFile: {0}", options.OutputFile);
-                    System.Console.WriteLine("TimeZone: {0}", options.TimeZone);
-
-                    TextWriter writer = System.Console.Out;
-
-                    TimeZoneInfo timeZone = TimeZoneHelper.GetTimeZone();
-                    if (!string.IsNullOrEmpty(options.TimeZone))
-                    {
-                        timeZone = TimeZoneHelper.GetSpecificTimeZone(options.TimeZone);
-                    }
-
-
-                    writer.WriteLine("Event Log Processing");
-                    IEventLogSystem eventLogSystem = new FileEventLogSystem(options.EventLogFile);
-
-                    new EventLogReportPack("EventLog.Details.xlsx", eventLogSystem).Render();
+                    DirectoryInfo dir = new DirectoryInfo(options.EventLogDirectory);
+                    files = dir.EnumerateFiles("*.evtx").Select(fi => fi.FullName).ToArray();
+                }
+                else if (fileExists)
+                {
+                    files = new []{options.EventLogFile};
+                }
+                else
+                {
+                    writer.WriteLine("Need to specify an event log file or directory\r\n{0}", HelpText.AutoBuild(options));
                 }
 
+                if (files.Length > 0)
+                {
+                    writer.WriteLine("Event Log Processing");
+                    foreach (string file in files)
+                    {
+                        writer.WriteLine(" EventLog: {0}", file);
+                    }
+                    IEventLogSystem eventLogSystem = new FileEventLogSystem(files);
+
+                    new EventLogReportPack(options.OutputFile, eventLogSystem).Render();
+                }
             }
 
         }
 
-        private static IReportWriter GetReportWriter(EventLogReaderOptions options)
-        {
-            string fileName = options.OutputFile ?? "output.xlsx";
-            IReportWriter reportWriter = new ExcelReportWriter(fileName);
-            return reportWriter;
-        }
     }
 }
